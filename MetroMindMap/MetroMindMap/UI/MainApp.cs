242af9f4fb034423c64891f4f59b980d446148ab
Bekti Galan <bektigprasetyo@gmail.com>
2016-11-02 02:01:05 +0700

    init commit

---
+++ MetroMindMap/MetroMindMap/UI/MainApp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MetroMindMap.UI
+{
+    public partial class MainApp : Form
+    {
+        public MainApp()
+        {
+            InitializeComponent();
+        }
+    }
+}
